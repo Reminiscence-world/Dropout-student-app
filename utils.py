a312f1311854ad8ade80f23e2631385d2686ccd5
JeetJShah21 <jeetshah2108@gmail.com>
2026-09-18 00:53:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,30 +1,36 @@
 import streamlit as st
+
+
 def add_sidebar_logo():
-    st.markdown(
-        """
-        <style>
-        .aetion-sidebar-logo {
-            font-family: Georgia, "Times New Roman", serif;
-            font-size: 1.8rem;
-            font-weight: 700;
-            letter-spacing: -0.04em;
-            margin-top: -10px;
-            margin-bottom: 25px;
-            padding-left: 5px;
-        }
+    with st.sidebar:
+        st.markdown(
+            """
+            <style>
 
-        .aetion-sidebar-logo .aeti {
-            color: #B8860B;
-        }
+            div.aetion-sidebar-logo {
+                font-family: Georgia, "Times New Roman", serif;
+                font-size: 1.8rem;
+                font-weight: 700;
+                letter-spacing: -0.04em;
+                margin-top: -8px;
+                margin-bottom: 25px;
+                padding-left: 5px;
+                line-height: 1.2;
+            }
 
-        .aetion-sidebar-logo .on {
-            color: #202F57;
-        }
-        </style>
+            div.aetion-sidebar-logo span.aeti {
+                color: #B8860B !important;
+            }
 
-        <div class="aetion-sidebar-logo">
-            <span class="aeti"><i>Aeti</i></span><span class="on">ON</span>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+            div.aetion-sidebar-logo span.on {
+                color: #202F57 !important;
+            }
+
+            </style>
+
+            <div class="aetion-sidebar-logo">
+                <span class="aeti"><i>Aeti</i></span><span class="on">ON</span>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
