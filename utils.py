7a1194412ee5c8d0bff8af400f86e086635c14fa
JeetJShah21 <jeetshah2108@gmail.com>
2026-09-18 01:10:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ def add_sidebar_logo():
 
         /* ON = black + normal */
         .aetion-sidebar-logo .on {
-            color: #000000 !important;
+            color: #4B5563 !important;
             font-style: normal !important;
         }
 
