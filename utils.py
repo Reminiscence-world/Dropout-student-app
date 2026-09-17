b7a34185b0e9880205ff0f497430732f6b9e0c86
JeetJShah21 <jeetshah2108@gmail.com>
2026-09-18 00:31:25 +0530

    Update utils.py

---
+++ utils.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+def add_sidebar_logo():
+    st.markdown(
+        """
+        <style>
+        .aetion-sidebar-logo {
+            font-family: Georgia, "Times New Roman", serif;
+            font-size: 1.8rem;
+            font-weight: 700;
+            letter-spacing: -0.04em;
+            margin-top: -10px;
+            margin-bottom: 25px;
+            padding-left: 5px;
+            color: #FFFFFF;
+        }
+
+        .aetion-sidebar-logo .aeti {
+            color: #FFFFFF;
+        }
+
+        .aetion-sidebar-logo .on {
+            color: #5EA4F3;
+        }
+        </style>
+
+        <div class="aetion-sidebar-logo">
+            <span class="aeti"><i>Aeti</i></span><span class="on">ON</span>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
